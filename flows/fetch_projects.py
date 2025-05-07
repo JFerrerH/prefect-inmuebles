38405dc73c602f708b21af1a20aaccac00a7b7a5
JFerrerH <132410631+JFerrerH@users.noreply.github.com>
2025-05-06 19:32:52 -0500

    initial prefect flow

---
+++ flows/fetch_projects.py
@@ -0,0 +1,47 @@
+from prefect import flow, task
+import pandas as pd
+import requests
+
+@task
+def fetch_data():
+    url = "https://new.informeinmobiliario.com/api/proyecto"
+    params = {
+        "concepto": 3,
+        "order": "rand",
+        "ciudad_proyecto": "",
+        "barrio_proyecto": "",
+        "pag": 1,
+        "results_per_page": 9
+    }
+    headers = {
+        "Authorization": "Basic aW5mb3JtZTppbmZvcm1lMjAyMUA=",
+        "Accept": "application/json",
+        "Origin": "https://www.informeinmobiliario.com",
+        "Referer": "https://www.informeinmobiliario.com/"
+    }
+
+    all_projects = []
+
+    while True:
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
+        projects = data.get("response", [])
+        if not projects:
+            break
+        all_projects.extend(projects)
+        params["pag"] += 1
+
+    return all_projects
+
+@task
+def save_to_json(projects):
+    df = pd.DataFrame(projects)
+    df.to_json("projects.json", index=False)
+
+@flow(name="fetch-inmuebles")
+def main_flow():
+    data = fetch_data()
+    save_to_json(data)
+
+if __name__ == "__main__":
+    main_flow()
